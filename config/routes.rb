94d03a24fdfeb13a499b078d1075891f61d9590e
Clementine Pirlot <clementine.pirlot@borrowell.com>
2019-04-08 08:45:55 -0400

    Authenticate user to access profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :profiles, only: [:index, :new, :create, :show, :edit]
+  authenticate :user do
+    resources :profiles, only: [:new, :create, :edit]
+  end
+  resources :profiles, only: [:index, :show]
   root to: "profiles#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
