a7368e8e038625b114545472da71fa6c00923dba
sweetyclem <pirlot.clementine@gmail.com>
2019-04-19 11:53:09 -0400

    Change after signup path location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,2 @@
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(resource)
-    # if current_user.has_role?(:admin)
-    #   dashboard_path
-    # elsif current_user.has_role?(:student)
-    #   root_path
-    # else
-      root_path
-  end
 end
