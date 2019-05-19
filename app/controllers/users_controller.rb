6129c56d7aabef88524e77791ebd25f324fab818
sweetyclem <pirlot.clementine@gmail.com>
2019-05-19 11:18:39 -0400

    Add search to active users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,13 @@ class UsersController < ApplicationController
     end
   end
 
+  # GET /users/active
+  def active
+    if current_user.admin
+      @users = User.includes(:profile).where(profiles: { active: true })
+    end
+  end
+
   def destroy
     @user = User.find(params[:id])
     if @user.id = current_user.id || current_user.admin
@@ -29,5 +36,23 @@ class UsersController < ApplicationController
       redirect_to root_url, notice: "User deleted."
     end
   end
+  
+    # POST /users/filter
+    def filter
+      if current_user.admin
+        @users = User.includes(:profile).where(profiles: { active: true })
+        if params[:search_term].present?
+          @users = @users.joins(:profile).where("firstName LIKE ?", "%#{params[:search_term]}%").or(@users.joins(:profile).where("lastName LIKE ?", "%#{params[:search_term]}%")).or(@users.joins(:profile).where("website LIKE ?", "%#{params[:search_term]}%"))
+        end
+        @users = @users.joins(:profile).where(profiles: { location: params[:location] }) if params[:location].present?
+        @users = @users.joins(:profile).where(profiles: { typeOfWork: params[:typeOfWork] }) if params[:typeOfWork].present?
+        @users = @users.joins(:profile).where(profiles: { fatCategory: params[:fatCategory] }) if params[:fatCategory].present?
+        @users = @users.joins(:profile).where(profiles: { orientation: params[:orientation] }) if params[:orientation].present?
+        @users = @users.joins(:profile).where(profiles: { religion: params[:religion] }) if params[:religion].present?
+        @users = @users.joins(:profile).where(profiles: { ethnicity: params[:ethnicity] }) if params[:ethnicity].present?
+        @showFilter = false
+        render "active"
+      end
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,11 @@ Rails.application.routes.draw do
   authenticated :user, lambda {|u| u.admin } do
     get 'activate/:id', to: 'users#activate', as: :activate_user
     get 'inactive', to: 'users#inactive', as: :inactive_users
+    get 'active', to: 'users#active', as: :active_users
     get 'destroy/:id', to: 'users#destroy', as: :destroy_user
   end
-  post 'filter', to: 'profiles#filter', as: :filter_profiles
+  post 'filterProfiles', to: 'profiles#filter', as: :filter_profiles
+  post 'filterUsers', to: 'users#filter', as: :filter_users
   root to: "profiles#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
