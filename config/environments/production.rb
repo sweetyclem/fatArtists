59af00e1b37bdb8c1e61d17808f5f02a776e047e
sweetyclem <pirlot.clementine@gmail.com>
2019-05-19 09:59:47 -0400

    Configure email sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: Rails.application.credentials.gmail_username,
+    password: Rails.application.credentials.gmail_password
+  }
+  
+  config.action_mailer.default_url_options = { :host => 'thefatfinder.org' }
 end
